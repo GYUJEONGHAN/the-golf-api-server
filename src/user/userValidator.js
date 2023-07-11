81f9b85ec47bce1bb02cc248ab27534b1fc4dac3
Suhyun <suhyun@suhyun.local>
2023-07-11 23:52:59 +0900

    User 모델의 Address 수정 및 발리데이터 수정

--- src/user/userValidator.js
+++ src/user/userValidator.js
@@ -11,7 +11,7 @@ const signUpValidator = Joi.object({
       "any.required": "비밀번호는 필수 입력 항목입니다.",
     }),
   email: Joi.string()
-    .email({ tlds: { allow: false } })
+    .email({ tlds: { allow: true } })
     .required()
     .messages({
       "string.email": "유효하지 않은 이메일 주소입니다.",
@@ -23,6 +23,19 @@ const signUpValidator = Joi.object({
     .messages({
       "string.pattern.base": "이름은 한글 2~4자여야 합니다.",
       "any.required": "이름은 필수 입력 항목입니다.",
+    }),
+  address: Joi.object({
+    streetAddress: Joi.string().required(),
+    detailAddress: Joi.string().required(),
+    postalCode: Joi.string().required().length(5),
+  }).required(),
+  birthdate: Joi.date().required(),
+  phoneNumber: Joi.string()
+    .required()
+    .pattern(/010-\d{4}-\d{4}/)
+    .messages({
+      "string.pattern.base": "휴대폰 번호 형식이 올바르지 않습니다.",
+      "any.required": "휴대폰 번호는 필수 입력 항목입니다.",
     }),
 });
 
